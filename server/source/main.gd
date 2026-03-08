974d0faa5b629bf6e0af1f054b3c15f77aeb57e9
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-03-08 19:50:36 -0300

    Startup do servidor e início do banco de dados

---
+++ server/source/main.gd
@@ -0,0 +1,40 @@
+extends Node
+class_name Main
+
+@export_category("Configuration")
+@export_group("Network")
+@export_range(1, 65535) var _port: int = 7720
+@export_range(1, 32) var _max_channels: int = 3
+@export_range(0, 60000, 1, "suffix:ms") var _connection_poll_time: int = 0
+@export_range(1, 4096) var _max_peers: int = 100
+@export_range(1, 10240) var _max_tasks: int = 2048
+@export_group("Database")
+@export_dir var _database_path: String = "user://server/"
+@export var _database_filename: String = "server"
+@export_range(0, 60000, 1, "suffix:ms") var _database_poll_time: int = 0
+
+var _database: Database
+var _network: Network
+
+
+func _ready() -> void:
+	_database = Database.new()
+	_database.initialize(
+		_database_path,
+		_database_filename
+	)
+
+	_network = Network.new()
+	_network.initialize(
+		_port,
+		_max_channels,
+		_max_peers,
+		_max_tasks
+	)
+
+
+func _process(_delta: float) -> void:
+	if _database:
+		_database.poll(_database_poll_time)
+	if _network:
+		_network.poll(_connection_poll_time)
---
+++ server/source/network/network.gd
@@ -0,0 +1,45 @@
+extends RefCounted
+class_name Network
+
+signal peer_connected(peer_id: int)
+signal peer_disconnected(peer_id: int)
+
+var _server: EnhancedServer
+
+
+func initialize(port: int, max_channels: int, max_peers: int, max_tasks: int) -> void:
+	_server = EnhancedServer.new(max_tasks)
+
+	print("[NETWORK] Iniciando o network na porta %d..." % port)
+
+	var peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+
+	var error: Error = peer.create_server(
+		port,
+		max_peers,
+		max_channels
+	)
+
+	if error != OK:
+		push_error("[NETWORK] Falha ao iniciar o network: %s" % error)
+		return
+
+	_server.start(peer)
+	print("[NETWORK] Network iniciado com capacidade de %d peers." % max_peers)
+
+	_server.peer_connected.connect(
+		func(peer_id: int):
+			print("[NETWORK] Novo peer conectado: %d" % peer_id)
+			peer_connected.emit(peer_id)
+	)
+
+	_server.peer_disconnected.connect(
+		func(peer_id: int):
+			print("[NETWORK] Peer desconectado: %d" % peer_id)
+			peer_disconnected.emit(peer_id)
+	)
+
+
+func poll(_connection_timeout: int) -> void:
+	if _server:
+		_server.poll(_connection_timeout)
