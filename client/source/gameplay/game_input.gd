231a8746078c63d107bfc7eee6d9d4fdcc8221ca
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-07-14 14:25:23 -0300

    Suporte ao joystick para mobile

--- client/source/gameplay/game_input.gd
+++ client/source/gameplay/game_input.gd
@@ -1,6 +1,9 @@
 extends Node
 
 
+const JOYSTICK_DEAD_ZONE: float = 0.5
+
+
 func _physics_process(_delta: float) -> void:
 	_handle_movement()
 
@@ -14,31 +17,19 @@ func _handle_movement() -> void:
 	if actor == null or actor.is_walking:
 		return
 
+	var input: Vector2 = Input.get_vector(
+		"walking_left",
+		"walking_right",
+		"walking_up",
+		"walking_down"
+	)
+
 	var direction: Vector2i = Vector2i.ZERO
 
-	if Input.is_action_pressed("walking_up"):
-		direction = Vector2i.UP
-	if Input.is_action_pressed("walking_down"):
-		direction = Vector2i.DOWN
-	if Input.is_action_pressed("walking_left"):
-		direction = Vector2i.LEFT
-	if Input.is_action_pressed("walking_right"):
-		direction = Vector2i.RIGHT
-
-	if direction == Vector2i.ZERO:
-		var joy_x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-		var joy_y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-		var joy_vec = Vector2(joy_x, joy_y)
-
-		if joy_vec.length() >= 0.2:
-			match rad_to_deg(joy_vec.angle()):
-				45:
-					direction = Vector2i.DOWN
-				135:
-					direction = Vector2i.LEFT
-				-135:
-					direction = Vector2i.UP
-				_:
-					direction = Vector2i.RIGHT
+	if input.length() >= JOYSTICK_DEAD_ZONE:
+		if abs(input.x) > abs(input.y):
+			direction = Vector2i.RIGHT if input.x > 0 else Vector2i.LEFT
+		else:
+			direction = Vector2i.DOWN if input.y > 0 else Vector2i.UP
 
 	actor.move(direction)
