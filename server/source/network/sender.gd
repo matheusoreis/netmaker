35e6b4574cef2ac4f2639d3aeab9e827a00e5c39
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-07-19 10:23:01 -0300

    Lógica inicial e sender inicial

--- server/source/network/sender.gd
+++ server/source/network/sender.gd
@@ -1,1 +1,25 @@
 extends Node
+
+
+func join() -> void:
+	pass
+
+
+func map_data() -> void:
+	pass
+
+
+func map_actors() -> void:
+	pass
+
+
+func move() -> void:
+	pass
+
+
+func left() -> void:
+	pass
+
+
+func chat_message() -> void:
+	pass
