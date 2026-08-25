0c527bea89a52fffe3d068199eeadd9224aa5152
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-08-25 13:47:43 -0300

    Base para os handlers

---
+++ server/source/network/handlers/auth.gd
@@ -0,0 +1,21 @@
+extends RefCounted
+class_name AuthHandler
+
+
+var _network: Network
+
+
+func _init(network: Network) -> void:
+	_network = network
+
+
+func register() -> Error:
+	return _network.register([
+
+	])
+
+
+func unregister() -> Error:
+	return _network.register([
+
+	])
---
+++ server/source/network/handlers/map.gd
@@ -0,0 +1,21 @@
+extends RefCounted
+class_name MapHandler
+
+
+var _network: Network
+
+
+func _init(network: Network) -> void:
+	_network = network
+
+
+func register() -> Error:
+	return _network.register([
+
+	])
+
+
+func unregister() -> Error:
+	return _network.register([
+
+	])
