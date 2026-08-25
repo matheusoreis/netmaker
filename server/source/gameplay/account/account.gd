111473fa6372297a2a671c9568169cc9cd6c6f9e
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-08-25 14:46:24 -0300

    Lógica da account finalizada

--- server/source/gameplay/account/account.gd
+++ server/source/gameplay/account/account.gd
@@ -1,2 +1,36 @@
 extends RefCounted
 class_name Account
+
+
+enum Role {
+	NONE,
+	MODERATOR,
+	ADMINISTRATOR
+}
+
+
+var id: int
+
+var email: String
+var password: String
+
+var role: Role
+
+var access_at: int
+var banned_at: int
+var created_at: int
+var updated_at: int
+
+
+func _init(id: int, email: String, password: String, role: Role, access_at: int, banned_at: int, created_at: int, updated_at: int) -> void:
+	self.id = id
+
+	self.email = email
+	self.password = password
+
+	self.role = role
+
+	self.access_at = access_at
+	self.banned_at = banned_at
+	self.created_at = created_at
+	self.updated_at = updated_at
