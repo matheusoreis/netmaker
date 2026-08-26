5b006246f24372754375221a2aef8b464c07ec61
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-08-26 10:14:34 -0300

    Base das Scenes

---
+++ client/source/scenes/scene.gd
@@ -0,0 +1,59 @@
+extends Node2D
+class_name Scene
+
+
+@export_category("Controls")
+@export var _interfaces: Dictionary[StringName, Control]
+
+
+func add_interface(identifier: StringName, interface: Control) -> void:
+	if is_instance_valid(interface) == false:
+		return
+
+	if _interfaces.has(identifier):
+		return
+
+	_interfaces[identifier] = interface
+
+
+func remove_interface(identifier: StringName) -> void:
+	var interface: Control = get_interface(identifier)
+	if interface == null:
+		return
+
+	_interfaces.erase(identifier)
+	interface.queue_free()
+
+
+func get_interface(identifier: StringName) -> Control:
+	return _interfaces.get(identifier, null)
+
+
+func toggle_interface(identifier: StringName) -> void:
+	var interface: Control = get_interface(identifier)
+	if interface == null:
+		return
+
+	interface.visible = not interface.visible
+
+	if interface.visible and interface is Control:
+		(interface as Control).bring_to_front()
+
+
+func show_interface(identifier: StringName) -> void:
+	var interface: Control = get_interface(identifier)
+	if interface == null:
+		return
+
+	interface.visible = true
+
+	if interface is Control:
+		(interface as Control).bring_to_front()
+
+
+func hide_interface(identifier: StringName) -> void:
+	var interface: Control = get_interface(identifier)
+	if interface == null:
+		return
+
+	interface.visible = false
