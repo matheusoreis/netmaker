7cb455edc3333e6336d2fe8fa8c59ee51af2418a
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-08-26 14:09:03 -0300

    Lógica inicial dos handlers

--- client/source/network/handlers/action.gd
+++ client/source/network/handlers/action.gd
@@ -4,11 +4,57 @@ class_name ActionHandler
 
 func register() -> Error:
 	return Network.register([
-
+		alert,
+		confirmation,
+		ammount
 	])
 
 
 func unregister() -> Error:
 	return Network.unregister([
-
+		alert,
+		confirmation,
+		ammount
 	])
+
+
+func alert(code: String) -> void:
+	var main: Main = get_tree().root.get_node("./Main")
+	if main == null:
+		return
+
+	var scene: Scene = main.current_scene
+	if scene == null:
+		return
+
+	# TODO: Mostrar a mensagem
+
+	scene.show_interface(&"Alert")
+
+
+func confirmation(code: String) -> void:
+	var main: Main = get_tree().root.get_node("./Main")
+	if main == null:
+		return
+
+	var scene: Scene = main.current_scene
+	if scene == null:
+		return
+
+	# TODO: Mostrar a mensagem
+
+	scene.show_interface(&"Confirmation")
+
+
+func ammount(code: String) -> void:
+	var main: Main = get_tree().root.get_node("./Main")
+	if main == null:
+		return
+
+	var scene: Scene = main.current_scene
+	if scene == null:
+		return
+
+	# TODO: Mostrar a mensagem
+
+	scene.show_interface(&"Ammount")
--- client/source/network/handlers/character.gd
+++ client/source/network/handlers/character.gd
@@ -4,11 +4,48 @@ class_name CharacterHandler
 
 func register() -> Error:
 	return Network.register([
-
+		list_characters,
+		create_character,
+		delete_character,
+		select_character
 	])
 
 
 func unregister() -> Error:
 	return Network.unregister([
-
+		list_characters,
+		create_character,
+		delete_character,
+		select_character
 	])
+
+
+func list_characters(characters: Array) -> void:
+	var main: Main = get_tree().root.get_node("./Main")
+	if main == null:
+		return
+
+	var menu: Menu = main.current_scene
+	if menu == null:
+		return
+
+	# TODO: Configurar a UI
+
+	menu.hide_interface(&"CharacterCreation")
+	menu.show_interface(&"CharacterSelection")
+
+
+func create_character() -> void:
+	Network.exec(&"list_characters")
+
+
+func delete_character() -> void:
+	Network.exec(&"list_characters")
+
+
+func select_character() -> void:
+	var main: Main = get_tree().root.get_node("./Main")
+	if main == null:
+		return
+
+	main.go_to_game()
