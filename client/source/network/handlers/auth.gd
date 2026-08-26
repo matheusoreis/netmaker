90bb524c242facadbb4698f7969cd7fadc27d8db
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2026-08-26 13:52:07 -0300

    Update auth.gd

--- client/source/network/handlers/auth.gd
+++ client/source/network/handlers/auth.gd
@@ -4,11 +4,44 @@ class_name AuthHandler
 
 func register() -> Error:
 	return Network.register([
-
+		sign_in,
+		sign_up
 	])
 
 
 func unregister() -> Error:
 	return Network.unregister([
-
+		sign_in,
+		sign_up
 	])
+
+
+func sign_in() -> void:
+	var main: Main = get_tree().root.get_node("./Main")
+	if main == null:
+		return
+
+	var menu: Menu = main.current_scene
+	if menu == null:
+		return
+
+	Network.exec(&"list_characters")
+
+	menu.hide_interface(&"SignIn")
+	menu.show_interface(&"Characterlist")
+
+
+
+func sign_up() -> void:
+	var main: Main = get_tree().root.get_node("./Main")
+	if main == null:
+		return
+
+	var menu: Menu = main.current_scene
+	if menu == null:
+		return
+
+	Network.exec(&"list_characters")
+
+	menu.hide_interface(&"SignUp")
+	menu.show_interface(&"Characterlist")
